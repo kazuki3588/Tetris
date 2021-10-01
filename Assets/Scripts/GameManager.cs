df8d99eb906191deb96c15f243b6abf8120fa7b8
kazuki3588 <kuramoto880@ezweb.ne.jp>
2021-10-01 22:39:45 +0900

    リスタートの実装

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
 //ゲームの状態を管理するクラス
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
     Text gameStateText;
+
     public void GameOver()
     {
         gameStateText.text = "ゲームオーバー";
+        StartCoroutine("ReStart");
+    }
+
+    IEnumerator ReStart()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
