d4506b4bd1f00b9decd2fcf1f2e063d9a5e52940
kazuki3588 <kuramoto880@ezweb.ne.jp>
2021-10-02 15:48:52 +0900

    Hold機能修正１

--- Assets/Scripts/Hold.cs
+++ Assets/Scripts/Hold.cs
@@ -7,11 +7,19 @@ class Hold : MonoBehaviour
 {
     GameObject[] currentMinos;//現在存在しているミノをすべて格納
     Vector3 holdPosition = new Vector3(12f,9.7f,0);
+    Vector3 middlePosition;//途中の位置を格納
     bool firstClick;
 
     public void PushHoldButton()
     {
         currentMinos = GameObject.FindGameObjectsWithTag("Mino");
+        foreach(var activeMino in currentMinos)
+        {
+            if(activeMino.GetComponent<Mino>().enabled == true)
+            {
+                middlePosition = activeMino.transform.position;//事前にポジション取得
+            }
+        }
         foreach(var _ctMino in currentMinos)
         {
             if (_ctMino.GetComponent<Mino>().enabled == true)
@@ -21,13 +29,13 @@ class Hold : MonoBehaviour
             }
             else if (_ctMino.GetComponent<Mino>().enabled == false && _ctMino.GetComponent<Mino>().NextFlag == false && _ctMino.GetComponent<Mino>().HoldFlag == true)
             {
-                _ctMino.transform.position = new Vector3(5f, 19f, 0f);
+                _ctMino.transform.position = middlePosition;
                 _ctMino.GetComponent<Mino>().enabled = true;
             }
             else if (_ctMino.GetComponent<Mino>().NextFlag == true && !firstClick)
             {
                 firstClick = true;
-                _ctMino.GetComponent<Mino>().transform.position = new Vector3(5f, 19f, 0);
+                _ctMino.GetComponent<Mino>().transform.position = middlePosition;
                 _ctMino.GetComponent<Mino>().enabled = true;
                 _ctMino.GetComponent<Mino>().NextFlag = false;
                 _ctMino.GetComponent<Mino>().HoldFlag = true;
